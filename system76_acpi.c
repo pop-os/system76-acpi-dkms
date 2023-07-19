e84f38390671d1cb58a7d7696e5dad147ae90286
Winston Hoy <winston@curiousercreative.com>
2023-07-19 15:22:44 -0400

    remove comment

--- system76_acpi.c
+++ system76_acpi.c
@@ -144,7 +144,6 @@ static int system76_set(struct system76_data *data, char *method, int value)
 	acpi_handle handle;
 	acpi_status status;
 
-	// log a message to dmesg when ACPI method is 
 	if (!acpi_has_method(acpi_device_handle(data->acpi_dev), method)) {
 		printk(KERN_WARNING "unsupported method: %s\n", method);
 	}
